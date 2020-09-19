a9cc260c72ecce3defbd976b86e2831ef40e487c
n4r1tahhhh <ddryutaro@gmail.com>
2020-09-20 08:18:30 +0900

    データベースに接続

---
+++ api/db.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+
+	"github.com/joho/godotenv"
+)
+
+func EnvLoad() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func ConnectDB() (*gorm.DB, error) {
+	// 環境変数の設定
+	EnvLoad()
+	user := os.Getenv("USER_NAME")
+	password := os.Getenv("USER_PASSWORD")
+	endpoint := os.Getenv("ENDPOINT")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	// DBと接続
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, endpoint, dbName)
+	return gorm.Open(mysql.Open(dsn), &gorm.Config{})
+}
--- api/server.go
+++ api/server.go
@@ -9,6 +9,12 @@ import (
 func main() {
 	e := echo.New()
 
+	// Database connection
+	db, err := ConnectDB()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+
 	// Routes
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
