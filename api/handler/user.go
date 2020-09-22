ba47073d919f81de044c7d9162c58b8b763bef34
n4r1tahhhh <ddryutaro@gmail.com>
2020-09-22 11:52:56 +0900

    user.goのコメントとエラーハンドリング

--- api/handler/user.go
+++ api/handler/user.go
@@ -44,8 +44,13 @@ func (h *Handler) Signup(c echo.Context) error {
 
 // GetUsers ユーザ一覧の取得
 func (h *Handler) GetUsers(c echo.Context) error {
+	// ユーザ一覧をDBからフェッチ
 	var users []model.User
-	h.DB.Find(&users)
+	if result := h.DB.Find(&users); result.Error != nil {
+		return result.Error
+	}
+
+	// 名前とEmailだけ返す
 	var results []Result
 	for _, v := range users {
 		results = append(results, Result{Name: v.Name, Email: v.Email})
