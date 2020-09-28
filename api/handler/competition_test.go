54d1576a849fc1966702fac93cd1046fa94090ed
n4r1tahhhh <ddryutaro@gmail.com>
2020-09-29 07:13:59 +0900

    大会の新規作成と一覧のテスト

---
+++ api/handler/competition_test.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCompetition(t *testing.T) {
+	// Setup(Create)
+	f := make(url.Values)
+	f.Set("name", "Taiko no Tatsujin competition")
+	req := httptest.NewRequest(http.MethodPost, "/competitions", strings.NewReader(f.Encode()))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationForm)
+	rec := httptest.NewRecorder()
+	c := E.NewContext(req, rec)
+
+	// Assertions(Create)
+	if assert.NoError(t, H.CreateCompetitions(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, "Created!", rec.Body.String())
+	}
+
+	// Setup(GetUsers)
+	req = httptest.NewRequest(http.MethodGet, "/competitions", nil)
+	rec = httptest.NewRecorder()
+	c = E.NewContext(req, rec)
+
+	// Assertions(GetUsers)
+	if assert.NoError(t, H.GetCompetitions(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Contains(t, rec.Body.String(), "Taiko no Tatsujin")
+	}
+}
